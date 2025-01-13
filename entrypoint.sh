5275bd6191b507bcdddd321a3271540301bf0b82
Buddy-Lee CELDRAN <44428671+BudNatsu@users.noreply.github.com>
2025-01-13 12:18:11 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 echo "Starting container openrefine"
+# initialize default values for parameters
+export REFINE_MEMORY=${REFINE_MEMORY:='1030M'}
+
+
+
 openrefine/refine -i 0.0.0.0 -d /mnt/refine -m ${REFINE_MEMORY}
